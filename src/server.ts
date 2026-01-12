2fa15c14f77cd583cf9fc6ef084799f85aa168b3
jrsn <jrsnleons@gmail.com>
2026-01-12 23:52:29 +0800

    Fix session cookie sameSite for cross-origin requests

--- src/server.ts
+++ src/server.ts
@@ -122,7 +122,7 @@ app.use(
         cookie: {
             secure: process.env.NODE_ENV === "production",
             maxAge: 30 * 24 * 60 * 60 * 1000,
-            sameSite: "lax",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             httpOnly: true,
         },
     })
