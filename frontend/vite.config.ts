71af120291fb58e5c377837335c4fca84f3cc77d
jrsn <jrsnleons@gmail.com>
2026-01-13 22:50:20 +0800

    access to env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
     define: {
         'import.meta.env.VITE_API_URL': JSON.stringify(
-            process.env.VITE_API_URL 
+            process.env.VITE_API_URL
         ),
     },
     plugins: [
